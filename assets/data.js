3d938ef2ad05abb58db8034ccb5c24aee0ca01bd
utibe inyangetuk <inyangutibz@gmail.com>
2022-08-03 08:53:44 +0100

    data

---
+++ assets/data.js
@@ -0,0 +1,108 @@
+export const largeCardSections = [
+	{
+		id: 1,
+		title: 'Top-rated Fire Extinguishers',
+		snippet:
+			'Users say these are the best fire extinguishers to rent if your house is burning down.',
+		cards: [
+			{
+				id: 19,
+				title: "Toronto's Portable 2L Fire Extinguisher",
+				snippet:
+					'This fire extinguisher saved homes from completely burning down. People that used it only had 98% of their homes burned down.',
+				image: 'extinguisher.jpg',
+			},
+			{
+				id: 20,
+				title: 'Empty Super Red Fire Extinguisher',
+				snippet:
+					"This is one of the most red fire extinguishers you will ever see. Super clean. It's pretty much useless otherwise.",
+				image: 'extinguisher.jpg',
+			},
+			{
+				id: 21,
+				title: 'Fire Extinguisher, Perfect for Pools',
+				snippet:
+					'If you ever find that your pool is on fire then this is the fire extinguisher for you. It has a 100% success rate.',
+				image: 'extinguisher.jpg',
+			},
+		],
+	},
+	{
+		id: 2,
+		title: 'luxury fire extinguishers',
+		snippet:
+			'Take down the fire lavishly in style with our luxury extinguishers',
+		cards: [
+			{
+				id: 22,
+				title: '1923 vintage Fire Extinguisher, Perfect for Pools',
+				snippet:
+					'If you ever find that your pool is on fire then this is the fire extinguisher for you. It has a 100% success rate.',
+				image: 'extinguisher.jpg',
+			},
+			{
+				id: 23,
+				title: '2003 vintage Fire Extinguisher, Perfect for Pools',
+				snippet:
+					'If you ever find that your pool is on fire then this is the fire extinguisher for you. It has a 100% success rate.',
+				image: 'extinguisher.jpg',
+			},
+			{
+				id: 24,
+				title: '1523 vintage Fire Extinguisher, Perfect for Pools',
+				snippet:
+					'If you ever find that your pool is on fire then this is the fire extinguisher for you. It has a 100% success rate.',
+				image: 'extinguisher.jpg',
+			},
+		],
+	},
+]
+export const smallCardSections = [
+	{
+		id: 1,
+		title: 'fire extinguishers in toronto',
+		cards: [
+			{
+				id: 1,
+				image: 'extinguisher.jpg',
+			},
+			{
+				id: 2,
+				image: 'extinguisher.jpg',
+			},
+			{
+				id: 3,
+				image: 'extinguisher.jpg',
+			},
+			{
+				id: 4,
+				image: 'extinguisher.jpg',
+			},
+			{
+				id: 5,
+				image: 'extinguisher.jpg',
+			},
+			{
+				id: 6,
+				image: 'extinguisher.jpg',
+			},
+			{
+				id: 7,
+				image: 'extinguisher.jpg',
+			},
+			{
+				id: 8,
+				image: 'extinguisher.jpg',
+			},
+			{
+				id: 9,
+				image: 'extinguisher.jpg',
+			},
+			{
+				id: 10,
+				image: 'extinguisher.jpg',
+			},
+		],
+	},
+]
