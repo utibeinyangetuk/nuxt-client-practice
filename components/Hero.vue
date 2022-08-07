f228ca020e5675112ea2ac90fb6d7239903bc9eb
utibe inyangetuk <inyangutibz@gmail.com>
2022-08-07 12:52:05 +0100

    formatting

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,44 +1,39 @@
 <template>
-  <div class="container">
-    <div class="wrapper">
-      <h1 class="header">find your fire extinguisher</h1>
-      <p class="snippets">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod, placeat tempora? Adipisci eos quod, alias quam
-        animi rem provident laudantium tempore totam possimus itaque cum labore! Incidunt excepturi eaque illum!
-      </p>
-      <button>start looking</button>
-    </div>
-    <img src="../assets/extinguisher.jpg" alt="fire extinguisher" />
-  </div>
+	<div class="container">
+		<div class="wrapper">
+			<h1 class="header">find your fire extinguisher</h1>
+			<p class="snippets">
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod,
+				placeat tempora? Adipisci eos quod, alias quam animi rem provident
+				laudantium tempore totam possimus itaque cum labore! Incidunt
+				excepturi eaque illum!
+			</p>
+			<button>start looking</button>
+		</div>
+		<img src="../assets/extinguisher.jpg" alt="fire extinguisher" />
+	</div>
 </template>
 
- <script>
-
-
-export default {
-
-}
+<script>
+export default {}
 </script>
-
-
-
 
 <style scoped>
 .container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-family: poppins;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-family: poppins;
 }
 
 .wrapper {
-  display: flex;
-  flex-direction: column;
-  width: 500px;
-  align-items: center;
+	display: flex;
+	flex-direction: column;
+	width: 500px;
+	align-items: center;
 }
 
 .container img {
-  width: 300px;
+	width: 300px;
 }
 </style>
